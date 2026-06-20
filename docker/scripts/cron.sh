de36c8c56a97c2ae5d99cbc69bca1b5c4c9ba39d
HomeServerHQ <118991087+homeserverhq@users.noreply.github.com>
2026-06-20 07:14:23 -0500

    Quiet down the cron logging

--- docker/scripts/cron.sh
+++ docker/scripts/cron.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-set -e
 
 echo "Starting SuiteCRM cron scheduler..."
 cd /var/www/html
 
 while true; do
-  su -s /bin/sh www-data -c "cd /var/www/html && bin/console schedulers:run --no-interaction" 2>&1 | \
-    awk "{ print strftime(\"[%Y-%m-%d %H:%M:%S]\"), \$0 }"
+  output=$(su -s /bin/sh www-data -c "cd /var/www/html && bin/console schedulers:run --no-interaction" 2>&1)
+  exit_code=$?
+  if [ $exit_code -eq 0 ] && ! echo "$output" | grep -q '(Failed)'; then
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] Schedulers completed."
+  else
+    echo "$output" | awk "{ print strftime(\"[%Y-%m-%d %H:%M:%S]\"), \$0 }"
+  fi
   sleep 60
 done
